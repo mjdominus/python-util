56f232715c315173005af9e17e6b012a640159eb
Mark Jason Dominus (陶敏修) <mjd@plover.com>
2018-02-13 09:48:22 -0500

    Atomic database updating
    You should add advisory locking at some point

--- medical-bills/billdb.py
+++ medical-bills/billdb.py
@@ -2,12 +2,22 @@
 # import html.parser
 import xml.etree.ElementTree as ET
 import yaml
+import os
 
 class billdb():
-    def __init__(self, file, rdonly=False, debug=False):
+    def __init__(self, path, rdonly=False, debug=False, tmpfile=None, backupfile=None):
         self.debug = debug
         self.rdonly = rdonly
-        self.file = self.maybe_open_file(file)
+        self.path = path
+        if tmpfile is None:
+            self.tmpfile = path + ".tmp"
+        else:
+            self.tmpfile = tmpfile
+        if backupfile is None:
+            self.backupfile = path + ".bkp"
+        else:
+            self.backupfile = backupfile
+        self.open_file(self.path)
         self.parser = ET
         self.tree = None
         self.db = {}
@@ -17,21 +27,14 @@ class billdb():
         if self.rdonly: return "r"
         else: return "r+"
 
-    def maybe_open_file(self, path_or_file, mode=None):
+    def open_file(self, path, mode=None):
         if mode is None: mode = self.mode()
-        file = path_or_file
-        try:
-            # see if it's actually a path
-            file = open(path_or_file, mode=mode)
-        except TypeError:
-            # If not, it should be something that works like a file
-            pass
-        return file
+        self.file = open(path, mode=mode)
 
     def import_xml(self, xmlfile):
         if self.rdonly:
             raise Exception("Can't import into read-only database")
-        f = self.maybe_open_file(xmlfile, mode='r')
+        f = open(xmlfile, mode='r')
         self.tree = self.parser.parse(f)
         count = 0
         for xml in self.root():
@@ -55,9 +58,21 @@ class billdb():
         self.db = db
         return self
 
+    # Trying hard to do this safely
     def save(self):
         if self.rdonly:
             raise Exception("Can't save to read-only database")
+        
+        self.save_copy(self.tmpfile)
+        try: os.unlink(self.backupfile)
+        except FileNotFoundError: pass
+        os.link(self.path, self.backupfile)
+        os.rename(self.tmpfile, self.path)
+
+    def save_copy(self, target_path):
+        import sys
+        f = open(target_path, mode="w")
+        yaml.dump(self.db, f, default_flow_style=False)
         return self
 
     def skippable_xml(self, xml):
@@ -135,7 +150,7 @@ class billdb():
         return "".join(elem.itertext())
 
 if __name__ == '__main__':
-    bdb = billdb("samples/arec.bdb", debug=True)
-    bdb.import_xml("samples/a.xls")
-    from pprint import pprint as pp
-    pp(vars(bdb))
+    bdb = billdb("samples/arec.bdb", debug=True) \
+        .import_xml("samples/a.xls").save()
+#    from pprint import pprint as pp
+#    pp(vars(bdb))
